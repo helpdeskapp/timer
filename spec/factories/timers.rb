a4b5ca7fc5a952dc8a07966995c7e104765d8397
tab <tukhfatullin@gmail.com>
2015-03-24 22:38:21 +0600

    Add stubs for timer spec

---
+++ spec/factories/timers.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :timer do
+    title "MyText"
+kind "MyString"
+amount 1
+active false
+start_at "2015-03-23 20:33:51"
+end_at "2015-03-23 20:33:51"
+user nil
+  end
+
+end
