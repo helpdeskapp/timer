fac090e347af3941ad2f90ddeac87c51b6d3ebce
tab <tukhfatullin@gmail.com>
2015-03-29 20:43:08 +0600

    Initial timer spec

--- spec/factories/timers.rb
+++ spec/factories/timers.rb
@@ -1,12 +1,14 @@
-FactoryGirl.define do
-  factory :timer do
-    title "MyText"
-kind "MyString"
-amount 1
-active false
-start_at "2015-03-23 20:33:51"
-end_at "2015-03-23 20:33:51"
-user nil
-  end
+require 'faker'
 
+FactoryGirl.define do
+  sec = Faker::Number.number(5)
+
+  factory :timer do
+    title { Faker::Lorem.word }
+    amount { sec }
+    start_at { Time.zone.now }
+    end_at { Time.zone.now + sec.to_i.seconds }
+
+    association :user, :factory => :user
+  end
 end
--- spec/models/timer_spec.rb
+++ spec/models/timer_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe Timer, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Timer do
+
+  timer = FactoryGirl.build(:timer)
+
+  describe "validations" do
+    subject { FactoryGirl.build(:timer) }
+
+    it { should validate_presence_of(:title) }
+    it { should be_valid }
+  end
+
+  it "#by_day" do
+    expect(timer.start_at.to_date).to eq Time.zone.now.to_date
+  end
+
+  it "#spend_time" do
+    expect(timer.spend_time).to eq timer.amount.to_i + Time.zone.now.to_i - timer.start_at.to_time.to_i
+  end
 end
